e57d48e2f6a198b6f018f109fc46f71079e868d8
sunjonwayne <wayne050611@hotmail.com>
2015-04-12 01:27:37 +0800

    test

---
+++ Test1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int a, b;
+	float ans;
+	value();
+	ans = sin(a) + cos(b);
+	printf( "%f \n", ans );
+
+	return 0;
+}
---
+++ Test1/value.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int value()
+{
+	int a, b;
+	a = 1;
+	b = 2;
+	return a, b;
+}
